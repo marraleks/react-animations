4bf114c8af8791fd16367bae892d2cfbb1f65fb9
Marius Aleksander Sletten <marraleks@hotmail.com>
2020-05-15 11:47:12 +0200

    added about animations

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,29 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Burger from "../components/Burger/Burger";
 import Header from "../components/Header/Header";
 import styled from "styled-components";
+import "./About.scss";
+import { gsap } from "gsap";
+
+const H1 = styled.h1`
+  margin-left: 20vw;
+  font-family: "IntegralCF";
+  letter-spacing: 20px;
+  color: black;
+`;
+
+const P = styled.p`
+  margin-left: 20vw;
+  margin-top: 50px;
+  margin-right: 5vw;
+  font-family: "IntegralCF";
+  letter-spacing: 40px;
+  color: black;
+`;
 
 const About = (props) => {
-  const H1 = styled.h1`
-    margin-left: 20vw;
-    margin-top: 10vh;
-    font-family: "IntegralCF";
-    letter-spacing: 20px;
-    color: black;
-  `;
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let lorem = useRef(null);
+  let background = useRef(null);
 
-  const P = styled.p`
-    margin-left: 20vw;
-    margin-top: 50px;
-    margin-right: 5vw;
-    font-family: "IntegralCF";
-    letter-spacing: 40px;
-    color: black;
-  `;
+  useEffect(() => {
+    gsap.to([background], 1, {
+      delay: 0,
+      ease: "power3.out",
+      width: "0",
+      stagger: {
+        amount: 0.15,
+      },
+    });
+
+    gsap.from([line1, line2], 0.8, {
+      delay: 0.3,
+      ease: "power3.out",
+      y: 65,
+      stagger: {
+        amount: 0.15,
+      },
+    });
+    gsap.from([lorem], 2, {
+      delay: 0.5,
+      ease: "power3.out",
+      opacity: "0",
+      stagger: {
+        amount: 0.15,
+      },
+    });
+  }, [line1, line2, lorem, background]);
 
   const [open, setOpen] = useState(false);
   return (
     <div>
+      <div
+        className="transition-background"
+        ref={(el) => (background = el)}
+      ></div>
       <Header open={open} hover={props.hover} unHover={props.unHover} />
       <Burger
         open={open}
@@ -31,15 +69,27 @@ const About = (props) => {
         hover={props.hover}
         unHover={props.unHover}
       />
-      <H1>This is an awesomeprojet</H1>
-      <P>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </P>
+
+      <div style={{ marginTop: "10vh" }}>
+        <div className="text-wrap">
+          <H1 ref={(el) => (line1 = el)}>This is an</H1>
+        </div>
+        <div className="text-wrap">
+          <H1 ref={(el) => (line2 = el)}>awesomeprojet.</H1>
+        </div>
+        <div>
+          <div className="text-wrap">
+            <P ref={(el) => (lorem = el)}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </P>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/screens/Fullscreen.js
+++ src/screens/Fullscreen.js
@@ -1,35 +1,46 @@
-import React, {useState} from 'react'
-import Burger from '../components/Burger/Burger'
-import Header from '../components/Header/Header'
-import Slideshow from '../components/Slideshow/Slideshow'
-import defaultSlides from '../components/Slideshow/defaultSlides.js'
-
-
+import React, { useState, useEffect, useRef } from "react";
+import Burger from "../components/Burger/Burger";
+import Header from "../components/Header/Header";
+import Slideshow from "../components/Slideshow/Slideshow";
+import defaultSlides from "../components/Slideshow/defaultSlides.js";
+import { gsap } from "gsap";
 
 const Fullscreen = (props) => {
-    const [open, setOpen] = useState(false)
-    return(
-        <div>
-            <Header
-                open={open}
-                hover={props.hover} 
-                unHover={props.unHover}
-            />
-            <Burger 
-                open={open}
-                setOpen={setOpen}
-                hover={props.hover} 
-                unHover={props.unHover}
-            />
-            <Slideshow
-                slides={defaultSlides}
-                open={open}
-                hover={props.hover} 
-                unHover={props.unHover}
-            />
-            
-        </div>
-    )
-}
+  const [open, setOpen] = useState(false);
+  let background = useRef(null);
 
-export default Fullscreen
+  useEffect(() => {
+    gsap.to([background], 1, {
+      delay: 0,
+      ease: "power3.out",
+      height: "0",
+      stagger: {
+        amount: 0.15,
+      },
+    });
+  }, [background]);
+
+  return (
+    <div>
+      <div
+        className="transition-background"
+        ref={(el) => (background = el)}
+      ></div>
+      <Header open={open} hover={props.hover} unHover={props.unHover} />
+      <Burger
+        open={open}
+        setOpen={setOpen}
+        hover={props.hover}
+        unHover={props.unHover}
+      />
+      <Slideshow
+        slides={defaultSlides}
+        open={open}
+        hover={props.hover}
+        unHover={props.unHover}
+      />
+    </div>
+  );
+};
+
+export default Fullscreen;
